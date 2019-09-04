ff9792996dbdb74002f56dd4780a975604812eb6
fumi uk <dcfumiya.238@gmail.com>
2019-09-05 06:18:02 +0900

    MyData
    ファイル削除

--- src/main/java/com/fumiya/springboot/MsgDataDao.java
+++
@@ -1,8 +0,0 @@
-package com.fumiya.springboot;
-
-import java.util.List;
-
-public interface MsgDataDao<T> {
-    public List<MsgData> getAll();
-    public MsgData findById(long id);
-}
--- src/main/java/com/fumiya/springboot/repositories/MsgDataRepository.java
+++ src/main/java/com/fumiya/springboot/repositories/MsgDataRepository.java
@@ -1,6 +1,5 @@
 package com.fumiya.springboot.repositories;
 
-import com.fumiya.springboot.MsgData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
