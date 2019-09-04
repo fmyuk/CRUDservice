b725aeddaeebbcf066238ed8457de7cdf8c706e5
fumi uk <dcfumiya.238@gmail.com>
2019-09-05 06:34:24 +0900

    Rest
    RestController作成

---
+++ src/main/java/com/fumiya/springboot/MyDataRestController.java
@@ -0,0 +1,24 @@
+package com.fumiya.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MyDataRestController {
+    @Autowired
+    private MyDataService service;
+
+    @RequestMapping("/rest")
+    public List<MyData> restAll() {
+        return service.getAll();
+    }
+
+    @RequestMapping("/rest/{num}")
+    public MyData restBy(@PathVariable int num) {
+        return service.get(num);
+    }
+}
